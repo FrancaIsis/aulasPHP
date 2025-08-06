ecffbe500fb00587067ff99f6cf54c4baed63f0e
FrancaIsis <isisdej@outlokk.com>
2025-08-05 21:40:22 -0300

    inicio atividade01 calculadora

---
+++ atividade01_op_aritmeticos/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Aritméticos</title>
+    <!-- Link para o CSS externo -->
+     <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Calculadora: Operadores Aritméticos</h1>
+    </header>
+    
+
+    <div class="caixa">
+        <form id="formCalculadora" method="POST" action="processa.php">
+            <label>Digite o primeiro número:</label>
+            <input type="text" id="num1" name="num1" required><br>
+            <label>Digite o segundo número:</label>
+            <input type="text" id="num2" name="num2" required><br>
+            <label>Escolha a operação:</label>
+            <select id="escolha" name="escolha">
+                <option value="soma">Soma (&plus;)</option>
+                <option value="subtracao">Subtração (&hybull;)</option>
+                <option value="multiplicacao">Multiplicação (&times;)</option>
+                <option value="divisao">Divisão (&divide;)</option>
+            <button type="submit">Calcular</button>
+        </form>
+    </div>
+
+    <!-- Link para o JavaScript externo -->
+     <script src="js/script.js"></script>
+</body>
+</html>
